cf34eb70cc4aa9f4b7b8ca2e0a76db3e3ceec4e3
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-11-08 20:34:01 -0300

    Create GlobalStyles

---
+++ src/components/GlobalStyles/index.js
@@ -0,0 +1,17 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import bgImg from '../../assets/bg.jpeg';
+
+export const GlobalStyles = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  body{
+    color: #332c36;
+    font-family: 'New Tegomin', serif;
+    background: url(${bgImg}) no-repeat center;
+    background-size: cover; 
+  }
+
+`
